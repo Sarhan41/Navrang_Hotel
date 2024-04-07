4b06950010bccae8559cc8c679fc646217bc45b2
Sarhan41 <sp.webdev2024@gmail.com>
2024-04-08 01:49:30 +0530

    Update viewport meta tag in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import Footer from '@/components/Footer/Footer';
 export const metadata: Metadata = {
   title: 'Navrang Sandwich & Pizza',
   description: 'Kill Your Hunger with Navrang Sandwich & Pizza',
+  viewport: 'width=device-width, initial-scale=1, maximum-scale=1'
 }
 
 export default function RootLayout({
