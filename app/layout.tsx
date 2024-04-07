52c6acdb531f1009f59da25068cf2f30003b152a
Sarhan41 <sp.webdev2024@gmail.com>
2024-04-07 21:05:35 +0530

    Update metadata title and description in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import Navbar from '@/components/Navbar/Navbar';
 import Footer from '@/components/Footer';
 
 export const metadata: Metadata = {
-  title: 'Travel',
-  description: 'Travel UI/UX App for Camping',
+  title: 'Navrang Sandwich & Pizza',
+  description: 'Kill Your Hunger with Navrang Sandwich & Pizza',
 }
 
 export default function RootLayout({
